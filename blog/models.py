dc216121390cc51411665d4685a1cf12d320ece0
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-03-16 16:59:42 +0000

    Setting up admin site with Django

--- blog/models.py
+++ blog/models.py
@@ -16,5 +16,5 @@ class Post(models.Model):
         def __unicode__(self):
             return u'%s'% self.title
 
-        def get_absolute_url(self):
-            return reverse('blog.views.post', args =["self.slug"])
+    def get_absolute_url(self):
+        return reverse('blog.views.post', args =["self.slug"])
